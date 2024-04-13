b1f78fd43c2fcc3f872c7f3fb0664e86f8c98da7
Dagmir01 <150508714+Dagmir01@users.noreply.github.com>
2024-04-13 18:17:45 +0300

    Add files via upload

---
+++ pithon_/work1/z4.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.speed(20)
+a=200
+
+while a>5:
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(a)
+    turtle.left(92)
+    turtle.forward(a)
+    turtle.left(90)
+    a=a-5
